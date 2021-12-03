ffb1c76537f79ac96417d79efd6d8eb5ecb748ba
Kiki <kmoniquetan@gmail.com>
2021-12-02 19:55:45 -0600

    tried to fix

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
-const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
-
 // Write your JavaScript code here!
+document = window.document;
 let list = "";
 let pilot = document.querySelector("input[name=pilotName]");
 let copilot = document.querySelector("input[name=copilotName]");
 let fuelLevel = document.querySelector("input[name=fuelLevel]");
 let cargoLevel = document.querySelector("input[name=cargoMass]");
 
-window.addEventListener("load", function() {
-    let form = document.querySelector("form");
-    form.addEventListener("submit", function(event) {
-        formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
-        alert("All fields required");
-        event.preventDefault()
-    }
-    ) 
+window.addEventListener("load", function () {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+    alert("all field required");
+    event.preventDefault();
+  });
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse = myFetch();
-   
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+  let listedPlanets;
+  // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+  let listedPlanetsResponse = myFetch();
+  listedPlanetsResponse
+    .then(function (result) {
+      listedPlanets = result;
+      console.log(listedPlanets);
+    })
+    .then(function () {
+      console.log(listedPlanets);
+      // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
 
-       let index = pickPlanet(listedPlanets);
-       addDestinationInfo(document, listedPlanets[index].name, listedPlanets[index].diameter, listedPlanets[index].star, listedPlanets[index].distance, listedPlanets[index].image, listedPlanets[index].moons);
-       console.log(listedPlanets[index], index);
-   })
-   
+      // Pick random planet from json
+      let index = pickPlanet(listedPlanets);
+      // Plug in json object and random planet
+      addDestinationInfo(
+        document,
+        listedPlanets[index].name,
+        listedPlanets[index].diameter,
+        listedPlanets[index].star,
+        listedPlanets[index].distance,
+        listedPlanets[index].moons,
+        listedPlanets[index].image
+      );
+      console.log(listedPlanets[index], index);
+    });
 });
